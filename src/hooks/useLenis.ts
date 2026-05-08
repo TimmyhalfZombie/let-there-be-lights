56103a9d89e32a10534263b68dc6090767ce1bc8
TimmyhalfZombie <shemuelrei@gmail.com>
2026-05-08 10:12:50 +0800

    feat: new About page

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -7,13 +7,13 @@ export function useLenis() {
     window.scrollTo(0, 0)
     
     const lenis = new Lenis({
-      duration: 1.8,
-      easing: (t: number) => 1 - Math.pow(1 - t, 4),
+      duration: 1.3,
+      easing: (t: number) => 1 - Math.pow(1 - t, 3),
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
-      wheelMultiplier: 0.8,
-      touchMultiplier: 1.6,
+      wheelMultiplier: 1.0,
+      touchMultiplier: 1.8,
       infinite: false,
     })
 
